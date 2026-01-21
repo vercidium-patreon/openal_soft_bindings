7206266818cdd2dbab1624d5c570b9f4fbd11c4a
Mitch RTX <mitchell@vercidium.com>
2026-01-21 16:11:35 +1100

    Added linux test project

---
+++ linux_test/Program.cs
@@ -0,0 +1,13 @@
+﻿using OpenAL;
+using System;
+
+namespace linux_test;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var deviceNames = AL.GetStringList(IntPtr.Zero, AL.ALC_ALL_DEVICES_SPECIFIER);
+        Console.WriteLine($"{deviceNames.Count} devices found");
+    }
+}
