b43c79599531c96bdd4e55a69dee5bb1a08420a0
Mitch RTX <mitchell@vercidium.com>
2026-02-05 10:48:41 +1100

    Fixed linux_test logging

--- linux_test/Program.cs
+++ linux_test/Program.cs
@@ -7,6 +7,8 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        OpenAL.Logger.Log = Console.WriteLine;
+        OpenAL.Logger.Error = Console.Error.WriteLine;
         var deviceNames = AL.GetStringList(IntPtr.Zero, AL.ALC_ALL_DEVICES_SPECIFIER);
         Console.WriteLine($"{deviceNames.Count} devices found");
     }
